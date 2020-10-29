689aa73c98803f6b0c9b62f1679d5d403a32a0d5
slyxdog <70235534+slyxdog@users.noreply.github.com>
2020-10-28 21:51:40 -0400

    Add files via upload
    Task: Take a previous assignment you missed points on and resubmit to earn back the points you missed.
    
    Please make sure to let us know in the comments of this assignment which assignment it is that we are regrading, or else we will not regrade.
    
    This goes for any previous assignment before this week (Except Practical).
    
    Submission: The URL to the assignment you are redoing.

---
+++ script.js
@@ -0,0 +1,34 @@
+var chrs = [
+  {name:"Optimus Prime", class: "leader", afl:"autobot", vehicle:"truck"},
+  {name:"Megatron", class: "leader", afl:"decepticon", vehicle:"tank"},
+  {name:"Bumblebee", class: "scout", afl:"autobot", vehicle:"car"},
+  {name:"Starscream", class: "scout", afl:"decepticon", vehicle:"plane"},
+  {name:"Ironhide", class: "soldier", afl:"autobot", vehicle:"truck"},
+  {name:"Brawl", class: "soldier", afl:"decepticon", vehicle:"tank"},
+];
+
+// do not write any elements into your HTML file
+
+// loop through the above array to append a div using .append() into the body tag
+
+// the appended div should always have the ".character" class
+
+// display the characters name, class, and vehicle in the div
+
+// Information should be displayed as "Name: Optimus Prime", with the next information on a different line
+
+// if the character being looped through has an afl of autobot/decepticon, give them either a class of "autobot", or "decepitcon"
+
+// in the top right, based on the top information, insert an img elemnt for either the autobot or decepticon
+
+// link for autobot image https://cdn4.iconfinder.com/data/icons/proglyphs-free/512/Autobot-512.png
+
+// link for decepticon image https://cdn4.iconfinder.com/data/icons/proglyphs-free/512/Decepticon-512.png
+
+// in the characters' "class" data, all of those values' letters should be capitalized when displayed in the div (so 'leader' should become 'LEADER')
+
+// in the characters' "vehicle" data, change the color of that value in the div based on its value
+// truck=blue
+// tank=green
+// card=gold
+// plane=white
